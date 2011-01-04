7ad682a0f7bdd24bab696168bcf6e5553e9cbbac
Jason May <jmay@pobox.com>
2011-01-03 16:01:47 -0800

    working?

--- lib/vmunger.rb
+++ lib/vmunger.rb
@@ -3,22 +3,23 @@ require "set"
 require "csv"
 require "facets" # for Hash#delete_values
 
-require "vmunger/array"
-require "vmunger/conversions"
-require "vmunger/hash"
-require "vmunger/object"
-require "vmunger/string"
-require "vmunger/symbol"
-require "vmunger/transformations"
-
 module VMunger
-  def reload!
+  def self.reload!
     $".grep(/vmunger/).each {|f| load(f)}
   end
 
-  def unmarshal(file)
+  def self.unmarshal(file)
     Marshal.load(File.read(file))
   end
 end
+
+require File.dirname(__FILE__) + "/vmunger/version"
+require File.dirname(__FILE__) + "/vmunger/array"
+require File.dirname(__FILE__) + "/vmunger/conversions"
+require File.dirname(__FILE__) + "/vmunger/hash"
+require File.dirname(__FILE__) + "/vmunger/object"
+require File.dirname(__FILE__) + "/vmunger/string"
+require File.dirname(__FILE__) + "/vmunger/symbol"
+require File.dirname(__FILE__) + "/vmunger/transformations"
 
 $stderr.puts "# loaded #{__FILE__}"
