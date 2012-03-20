364f065d4908abdd5b4518f88cee0fe8ab2aaef6
Jason May <jmay@pobox.com>
2012-03-20 11:05:33 -0700

    matching initials

--- lib/data_tools/comparator.rb
+++ lib/data_tools/comparator.rb
@@ -1,6 +1,17 @@
+# MULTI-MATCHING via components
+# go through all users
+# group by distinct sets of components
+# pick a (small) subset of component-keys, say <10.  Maybe random sample?
+# build a set of matching rules
+# run the subset * the full corpus * the matching rules
+
 class Comparator
-  def crunch(record, corpus)
-    corpus.delete_if{|r| r == record}.reduce([]) do |matches,candidate|
+  def initialize(corpus)
+    @corpus = corpus
+  end
+
+  def crunch(record)
+    @corpus.delete_if{|r| r == record}.reduce([]) do |matches,candidate|
       if evaluate(record, candidate)
         matches << candidate
       end
@@ -20,14 +31,34 @@ class Comparator
   #   a == b
   # end
 
-  # ignore anything with just initials
-  # must have at least 2 long components in each
+  # must have at least 2 long (non-initial-only) components in each
   # those long parts must be identical
+  # only one of the names can have any initials
   def missing_initials(a,b)
     longnames_a = a.select {|s| s.length > 1}
     longnames_b = b.select {|s| s.length > 1}
+    inits_a = a.select {|s| s.length == 1}
+    inits_b = b.select {|s| s.length == 1}
 
-    longnames_a.count >= 2 && longnames_b.count >= 2 && longnames_a == longnames_b
+    longnames_a.count >= 2 && longnames_b.count >= 2 && longnames_a == longnames_b && (inits_a.empty? || inits_b.empty?)
   end
 
+  # must have at least 1 long (non-initial-only) component in each
+  # those long parts must be identical
+  # all initials should correspond to non-matched longnames in the other input
+  def matching_initials(a,b)
+    longnames_a = a.select {|s| s.length > 1}
+    longnames_b = b.select {|s| s.length > 1}
+    inits_a = a.select {|s| s.length == 1}
+    inits_b = b.select {|s| s.length == 1}
+
+    return false unless longnames_a.count >= 1 && longnames_b.count >= 1
+
+    unmatched_longnames_a = longnames_a - longnames_b
+    unmatched_longnames_b = longnames_b - longnames_a
+    unmatched_inits_a = unmatched_longnames_a.map {|s| s[0]}
+    unmatched_inits_b = unmatched_longnames_b.map {|s| s[0]}
+
+    inits_a == unmatched_inits_b && inits_b == unmatched_inits_a
+  end
 end
--- spec/comparator_spec.rb
+++ spec/comparator_spec.rb
@@ -2,7 +2,15 @@ require_relative "spec_helper"
 
 describe "Comparator" do
   def explode(name)
-    name.split.sort.map(&:upcase)
+    name.split.map(&:upcase).sort
+  end
+
+  def try(rule, name1, name2)
+    @comp.send(rule, explode(name1), explode(name2)).should be_true
+  end
+
+  def bust(rule, name1, name2)
+    @comp.send(rule, explode(name1), explode(name2)).should be_false
   end
 
   before :all do
@@ -11,12 +19,29 @@ describe "Comparator" do
       "francis l palmer",
       "michael palmer"
     ]
-    @corpus = names.map {|name| explode(name)}
+    corpus = names.map {|name| explode(name)}
+    @comp = Comparator.new(corpus)
   end
 
-  it "works 1" do
-    c = Comparator.new
-    matches = c.crunch(explode("michael palmer"), @corpus)
+  it "finds names that match without initials" do
+    try(:missing_initials, "michael palmer", "michael g palmer")
+    try(:missing_initials, "michael palmer", "Q michael palmer")
+    try(:missing_initials, "michael palmer", "Michael N Palmer x")
+    bust(:missing_initials, "michael palmer", "Michael P")
+
+    matches = @comp.crunch(explode("michael palmer"))
     matches.should == [explode("michael g palmer")]
+  end
+
+  it "finds names that match initials to names" do
+    try(:matching_initials, "fred jones", "f jones")
+    try(:matching_initials, "fred jones", "jones f")
+    try(:matching_initials, "fred jones", "fred j")
+    try(:matching_initials, "fred xavier jones", "fred x jones")
+    try(:matching_initials, "fred xavier jones", "xavier jones f")
+    bust(:matching_initials, "fred xavier jones", "fred jones")
+    bust(:matching_initials, "fred xavier jones", "fred q jones")
+    bust(:matching_initials, "fred x jones", "fred q jones")
+    bust(:matching_initials, "fred xavier jones", "homer simpson")
   end
 end
