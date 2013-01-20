33020e7ad86f0c15eb5c23aae481e4666a617c34
Jason W. May <jmay@pobox.com>
2013-01-19 21:16:19 -0800

    gems

--- spec/import_spec.rb
+++ spec/import_spec.rb
@@ -5,7 +5,11 @@ require "json"
 describe "File Import" do
   it "imports" do
     f = File.open(File.dirname(__FILE__) + "/../hrhead.csv")
-    sio = f.import.csvme(StringIO.new, ['Person Phone GUID', 'Person Address GUID'])
-    puts sio.string
+    # sio = f.import.csvme(StringIO.new, ['Person Phone GUID', 'Person Address GUID'])
+    # puts sio.string
+
+    f.import.each_slice(3) do |slice|
+      puts slice.pluck('Person Phone GUID', 'Person Address GUID').to_json
+    end
   end
 end
